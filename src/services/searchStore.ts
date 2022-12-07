a1eccc25171bfe73ee70ac5498bba4a4285817ff
Kieran Langton <polygonr@gmail.com>
2022-12-07 19:35:09 +0000

    Fix store type name and currying for TypeScript

--- src/services/searchStore.ts
+++ src/services/searchStore.ts
@@ -1,11 +1,11 @@
 import create from "zustand";
 
-interface SearchTerState {
+interface SearchTermState {
   searchTerm: string;
   setSearchTerm: (searchTerm: string) => void;
 }
 
-export const useSearchTermStore = create<SearchTerState>((set) => ({
+export const useSearchTermStore = create<SearchTermState>()((set) => ({
   searchTerm: "",
   setSearchTerm: (searchTerm: string) => set(() => ({ searchTerm })),
 }));
